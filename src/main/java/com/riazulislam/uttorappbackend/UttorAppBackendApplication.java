3664492039f3596bda6e82af6a293b26ecd6a58d
Riaz-404 <riazulislam.cuc@gmail.com>
2025-08-13 07:13:23 +0600

    initializing the project

---
+++ src/main/java/com/riazulislam/uttorappbackend/UttorAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.riazulislam.uttorappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UttorAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UttorAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riazulislam/uttorappbackend/UttorAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riazulislam.uttorappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UttorAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
